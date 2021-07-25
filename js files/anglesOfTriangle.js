6b9f1cb0fd476b1f552a07e0b5b5aa0d69f298d0
Varnita <varnita.makrariya@gmail.com>
2021-07-25 22:55:37 +0530

    feat: added angles quiz

---
+++ js files/anglesOfTriangle.js
@@ -0,0 +1,29 @@
+const inputAngle = document.querySelectorAll(".inputAngle");
+const button = document.querySelector("#submitBtn");
+const formTriangle = document.querySelector("#formTriangle");
+const output = document.querySelector("#isTriangle");
+
+var angles=[];
+
+button.addEventListener('click', ()=>{
+    for(i=0;i<inputAngle.length;i++){
+    angles[i] = Number(inputAngle[i].value);
+    }
+
+    let sum = 0;
+    console.log(angles);
+
+    angles.map((angle)=>{
+        sum = sum + angle;
+    })
+
+    if(sum==180){
+        output.innerText = "Yess! these angles can make a triangle";
+    }
+
+    else{
+        output.innerText = "Noo! these angles cannot make a triangle. Please try another set.."
+    }
+})
+
+
